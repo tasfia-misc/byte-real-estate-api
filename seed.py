8cebb8542549463e242ebf87e0f43b996fb5a057
julissamackey <julissamackey@gmail.com>
2017-04-19 13:29:48 -0400

    seeded db

--- seed.py
+++ seed.py
@@ -1,12 +1,16 @@
 import config
 from model import Agents, Listings
+import datetime
 
 config.db.drop_all()
 config.db.create_all()
 
+current_time = datetime.datetime.now()
+
 first_user = Agents('john','smith','jsmith@gmail.com','2125559987','Nyc Realty', '123456asdfg')
 
+first_listing = Listings('295 Madison Ave 35th floor','New York City','New York', '$15,000/month', '10,000 sq ft', '5','2','Door Man', 'None', current_time, 'Rental','Sold')
+
+config.db.session.add(first_listing)
 config.db.session.add(first_user)
 config.db.session.commit()
-
-first_listing = Listings('295 Madison Ave 35th floor','New York City','New York',  )
