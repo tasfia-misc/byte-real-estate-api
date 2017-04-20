8efe0303c16f7407d1e97980d9aac067887fdf42
julissamackey <julissamackey@gmail.com>
2017-04-20 19:03:45 -0400

    first complete POST request method. Now to update DB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 from config import db, app
 from model import *
+import datetime
 
 @app.route('/<token>', methods = ['GET'])
 def agent_listings(token):
@@ -9,7 +10,28 @@ def agent_listings(token):
 		return jsonify(
 			listings = agent_listings
 		)
-
+@app.route('/<token>', methods = ['POST'])		
+def add_listing(token):	
+	new_listing = {
+	'street_address': request.json['street_address'],
+	'city' : request.json['city'],
+	'state': request.json['state'],
+	'zip' : request.json['zip'],
+	'price': request.json['price'],
+	'num_of_bedrooms': request.json['num_of_bedrooms'],
+	'num_of_bathrooms' : request.json['num_of_bathrooms'],
+	'amenities': request.json['amenities'],
+	'description' : request.json['description'],
+	'date_listed': datetime.datetime.now(),
+	'rental_or_sale' : request.json['rental_or_sale'],
+	'available or sold': request.json['available_or_sold'],
+	'agent_token' : token,
+	'square_feet': request.json['square_feet']
+	}
+	return jsonify({
+	"new_listing" :  new_listing
+	})
+		
 			
 		
 @app.route('/<token>/city=<city>', methods = ['GET'])
@@ -75,9 +97,6 @@ def availablity(token, availablity):
 		listing = availablity_results
 	)
 
-@app.route('/<token>/add-listing', methods=['POST'])
-def add_listing():
-	pass
 
 if __name__ == "__main__":
     app.run(debug=True)
